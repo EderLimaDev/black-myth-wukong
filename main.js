58fab4bee0efcc12191ffcd19de7c383a6087d02
EderLimaDev <ederlimarh@gmail.com>
2024-08-18 18:47:37 -0300

    chore: ajustes design e cores

--- main.js
+++ main.js
@@ -1,6 +1,23 @@
-const navbar = document.querySelector('.navbar-stores');
+const navbar = document.querySelector('.sidebar-stores');
 const btnBuy = document.querySelector('#btn-buyNow');
 
 btnBuy.addEventListener("click", () => {
     navbar.classList.toggle("open");
 })
+
+const cardImages = document.querySelector('.card-images');
+const cardItem = document.querySelector('.card-images img');
+
+let count = 1;
+
+function carrossel() {
+    count++
+    if(count > 4) {
+        count = 1;
+    }
+    let imgSource = `./assets/imgs/slide-${count}.png`
+    cardItem.setAttribute("src", imgSource);
+
+}
+
+setInterval(carrossel, 3000);
